f9c8b10ae057bc3d9798557269cc34d0fa38fcf5
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2022-10-19 01:57:33 +0800

    Added login and capabilities components

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  login: FormGroup | any;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.login = new FormGroup({
+      fname: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  logindata(login: FormGroup) {
+    console.log(this.login.value);
+  }
+}
